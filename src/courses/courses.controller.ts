aa183857eb7555694d29af880e3b77009ef776f9
RafaelMunizz <rafaelvictormuniz@gmail.com>
2022-12-14 17:57:43 -0300

    Updates

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,24 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
-import { Body, Param, Post } from '@nestjs/common/decorators';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Body, Delete, HttpCode, Param, Patch, Post, Res } from '@nestjs/common/decorators';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
 
+    // Singleton: Apenas uma instância 
+    constructor(private readonly coursesService: CoursesService) {}
+
     // exibindo a partir da rota /courses
     @Get()
     findAll(){
-        return 'Listagem de cursos';
+        //return response.status(200).send('Listagem de cursos'); // Mostrando na tela e enviando a resposta 200 do http
+        return this.coursesService.findAll();
     }
 
     // Pegando coisas do id
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return `Curso #${id}`;
+        return this.coursesService.findOne(id);
     }
 
-    // Pegando coisas específicas do body
+    // Pegando coisas específicas do body, no caso o 'name'
     @Post()
-    create(@Body('name') body) {
-        return body;
+    //@HttpCode(HttpStatus.NO_CONTENT) // Mandar resposta http da requisição (NO_CONTENT = Sem conteúdo)
+    create(@Body() body) {
+        return this.coursesService.create(body);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        return this.coursesService.update(id, body);
+    }
+
+    // Pegando coisas do id
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.coursesService.remove(id);
     }
 }
---
+++ src/courses/courses.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common/enums';
+import { HttpException } from '@nestjs/common/exceptions';
+import { Course } from './entities/course.entity';
+
+@Injectable()
+export class CoursesService {
+
+    // visibilidade nome: tipo
+    private courses: Course[] = [
+        {
+            id: 1,
+            name: "Fundamentos do framework NesJS",
+            description: "Fundamentos do framework NesJS",
+            tags: ["node.js", "nest.js", "Javascript"],
+        },
+    ];
+
+    findAll() {
+        return this.courses;
+    }
+
+    findOne(id: string) {
+        // No atributo 'courses', percorra com uma variável até onde o id recebido
+        // como argumento é igual a o que ele possui e o retorne 
+        const course =  this.courses.find((course: Course) => course.id === Number(id));
+
+        if (!course) {
+            throw new HttpException(`Course ID ${id} not found`, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Dto => Data Transfer object | Geralmente usado quando se vai receber dados para criação e atualização
+    create(createCourseDto: any){
+        this.courses.push(createCourseDto); // Inserindo novo dado 
+    }
+
+    update(id: string, updateCourdeDto: any){
+
+        // Index do id que se deve alterar
+        const indexCourse = this.courses.findIndex(
+            (course: Course) => course.id === Number(id),
+        );
+
+        // Atualizando posição com informações
+        this.courses[indexCourse] = updateCourdeDto;
+    }
+
+    remove(id: string) {
+
+        // Index do id que se deve alterar
+        const indexCourse = this.courses.findIndex(
+            (course: Course) => course.id === Number(id),
+        );
+
+        // Se o índice for maior que 0, exclua o objeeto da posição 1 vez
+        if (indexCourse >= 0) {
+            this.courses.splice(indexCourse, 1);
+        }
+    }
+}
