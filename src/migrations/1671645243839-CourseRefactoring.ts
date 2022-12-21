6bf5827bd9e976e701a45bec0f7ec3c9486bf71c
RafaelMunizz <rafaelvictormuniz@gmail.com>
2022-12-21 16:20:04 -0300

    Updates docker

--- src/migrations/1671645243839-CourseRefactoring.ts
+++ src/migrations/1671645243839-CourseRefactoring.ts
@@ -1,18 +1,17 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CourseRefactoring1671552206852 implements MigrationInterface {
+export class CourseRefactoring1671645243839 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
             `ALTER TABLE "courses" RENAME COLUMN "name" TO "course"`,
-        );
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
             `ALTER TABLE "courses" RENAME COLUMN "course" TO "name"`,
-        );
-        
+        )
     }
 
 }
